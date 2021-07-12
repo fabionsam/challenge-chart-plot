42417423bdb1315236c72d86de29aad570d61f82
Fábio Mattos <fmattos@tasken.com>
2021-07-11 22:21:27 -0300

    Adding initial value to text state for test. Adding condition to check if is testing or not

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,18 @@ import { MainContainer, Button, Footer, Header } from './styles'
 import DataParser from './utils/dataParser'
 
 const App: React.FC = () => {
-    const [text, setText] = useState('')
+    const [text, setText] =
+        useState(`{type: 'start', timestamp: 1519862400000, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']}
+{type: 'span', timestamp: 1519862400000, begin: 1519862400000, end: 1519862460000}
+{type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+{type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'chrome', min_response_time: 0.2, max_response_time: 1.2}
+{type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.2}
+{type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'firefox', min_response_time: 0.1, max_response_time: 1.0}
+{type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.2, max_response_time: 0.9}
+{type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.0}
+{type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'firefox', min_response_time: 0.2, max_response_time: 1.1}
+{type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.4}
+{type: 'stop', timestamp: 1519862400000}`)
     const [chartData, setChartData] = useState<Array<ChartData>>([])
 
     const handleCodeTextAreaOnChange = (
@@ -24,10 +35,18 @@ const App: React.FC = () => {
     return (
         <MainContainer>
             <Header>Fábio&apos;s Challenge</Header>
-            <CodeTextArea onChange={handleCodeTextAreaOnChange} />
+            {process.env.NODE_ENV !== 'test' && (
+                <CodeTextArea onChange={handleCodeTextAreaOnChange} />
+            )}
             <LineChart data={chartData} />
             <Footer>
-                <Button onClick={handleButtonClick}>Generate Chart</Button>
+                <Button
+                    id="generateChartButton"
+                    name="generateChartButton"
+                    onClick={handleButtonClick}
+                >
+                    Generate Chart
+                </Button>
             </Footer>
         </MainContainer>
     )
