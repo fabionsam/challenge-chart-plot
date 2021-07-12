e022463ec74c9a952eaddd5808212ca8dea8efd1
Fábio Mattos <fmattos@tasken.com>
2021-07-11 21:07:41 -0300

    Declaring types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,30 @@
 declare interface ChartData {
     id: any;
+    color?: string;
     data: Array<{ x: any; y: any }>;
+}
+
+declare interface StartSet {
+    type: 'start';
+    timestamp: number;
+    select: Array<string>;
+    group: Array<string>;
+}
+
+declare interface SpanSet {
+    type: 'span';
+    timestamp: number;
+    begin: number;
+    end: number;
+}
+
+declare interface StopSet {
+    type: 'stop';
+    timestamp: number;
+}
+
+declare interface DataSet {
+    type: 'data';
+    timestamp: number;
+    [key: string]: any;
 }
