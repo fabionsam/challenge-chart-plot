06469752646e9040b7f57111e29a0701bc07098d
Fábio Mattos <fmattos@tasken.com>
2021-07-11 22:21:33 -0300

    Adding some tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import {
+    render,
+    screen,
+    queryByAttribute,
+    getByRole,
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('check if title was rendered', () => {
+    render(<App />)
+    const element = screen.getByText("Fábio's Challenge")
+    expect(element).toBeInTheDocument()
+})
+
+test('check if generate chart button was rendered', () => {
+    const getById = queryByAttribute.bind(null, 'id')
+    const dom = render(<App />)
+    const element = getById(dom.container, 'generateChartButton')
+    expect(element).toBeInTheDocument()
+})
+
+test('check if chartContainer was rendered', () => {
+    const getById = queryByAttribute.bind(null, 'id')
+    const dom = render(<App />)
+    const element = getById(dom.container, 'chartContainer')
+    expect(element).toBeInTheDocument()
+})
+
+test('check if generate chart button click works well', () => {
+    render(<App />)
+    const element = screen.getByRole('button')
+    userEvent.click(element)
+    expect(element).toBeInTheDocument()
+})
